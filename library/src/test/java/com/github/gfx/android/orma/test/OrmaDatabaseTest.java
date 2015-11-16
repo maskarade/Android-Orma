23eb38c7b7b67c41c7dbb8727e7a079f983194d9
FUJI Goro (gfx) <goro-fuji@cookpad.com>
2015-11-16 14:41:31 +0900

    run tests with robolectric

--- library/src/test/java/com/github/gfx/android/orma/test/OrmaDatabaseTest.java
+++ library/src/test/java/com/github/gfx/android/orma/test/OrmaDatabaseTest.java
@@ -1,19 +1,25 @@
 package com.github.gfx.android.orma.test;
 
+import com.github.gfx.android.orma.BuildConfig;
 import com.github.gfx.android.orma.test.model.OrmaDatabase;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, manifest = Config.NONE)
 public class OrmaDatabaseTest {
 
     Context getContext() {
-        return InstrumentationRegistry.getTargetContext();
+        return RuntimeEnvironment.application;
     }
 
     @Test
--- library/src/test/java/com/github/gfx/android/orma/test/ReservedWordsTest.java
+++ library/src/test/java/com/github/gfx/android/orma/test/ReservedWordsTest.java
@@ -1,24 +1,30 @@
 package com.github.gfx.android.orma.test;
 
+import com.github.gfx.android.orma.BuildConfig;
 import com.github.gfx.android.orma.test.model.OrmaDatabase;
 import com.github.gfx.android.orma.test.model.Where;
 
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, manifest = Config.NONE)
 public class ReservedWordsTest {
 
     OrmaDatabase db;
 
     Context getContext() {
-        return InstrumentationRegistry.getTargetContext();
+        return RuntimeEnvironment.application;
     }
 
     @Before
