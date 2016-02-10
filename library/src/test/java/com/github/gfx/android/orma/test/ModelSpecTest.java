daff8c6e3bba09ce59e63ef7a29a88c88125a64f
FUJI Goro (gfx) <gfuji@cpan.org>
2016-02-10 22:44:56 +0900

    refactor tests

--- library/src/test/java/com/github/gfx/android/orma/test/ModelSpecTest.java
+++ library/src/test/java/com/github/gfx/android/orma/test/ModelSpecTest.java
@@ -45,6 +45,7 @@ import java.util.Currency;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.UUID;
 
 import static org.hamcrest.MatcherAssert.*;
@@ -120,19 +121,17 @@ public class ModelSpecTest {
         final UUID uuid = UUID.randomUUID();
         final BigDecimal bd = BigDecimal.valueOf(Long.MAX_VALUE).add(BigDecimal.ONE);
         final BigInteger bi = BigInteger.valueOf(Long.MAX_VALUE).add(BigInteger.TEN);
+        final List<String> collection = Arrays.asList("foo", "bar baz", "42", "true");
 
         ModelWithTypeAdapters model = db.createModelWithTypeAdapters(new ModelFactory<ModelWithTypeAdapters>() {
             @NonNull
             @Override
             public ModelWithTypeAdapters call() {
                 ModelWithTypeAdapters model = new ModelWithTypeAdapters();
-                model.list = Arrays.asList("foo", "bar", "baz");
-                model.arrayList = new ArrayList<>(model.list);
-                model.set = new LinkedHashSet<>();
-                model.set.add("foo");
-                model.set.add("bar");
-                model.set.add("baz");
-                model.hashSet = new HashSet<>(model.set);
+                model.list = collection;
+                model.arrayList = new ArrayList<>(collection);
+                model.set = new LinkedHashSet<>(collection);
+                model.hashSet = new HashSet<>(collection);
                 model.uri = Uri.parse("http://example.com");
                 model.date = new Date(now);
                 model.sqlDate = new java.sql.Date(now);
@@ -148,10 +147,10 @@ public class ModelSpecTest {
             }
         });
 
-        assertThat(model.list, contains("foo", "bar", "baz"));
-        assertThat(model.arrayList, contains("foo", "bar", "baz"));
-        assertThat(model.set, containsInAnyOrder("foo", "bar", "baz"));
-        assertThat(model.hashSet, containsInAnyOrder("foo", "bar", "baz"));
+        assertThat(model.list, contains(collection.toArray()));
+        assertThat(model.arrayList, contains(collection.toArray()));
+        assertThat(model.set, containsInAnyOrder(collection.toArray()));
+        assertThat(model.hashSet, containsInAnyOrder(collection.toArray()));
         assertThat(model.uri, is(Uri.parse("http://example.com")));
         assertThat(model.date, is(new Date(now)));
         assertThat(model.sqlDate.toString(), is(new java.sql.Date(now).toString()));
